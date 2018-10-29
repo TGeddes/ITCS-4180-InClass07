5e19e19417d538ccfcf980151d0639076c4ff689
Thomas <tgeddes@uncc.edu>
2018-10-29 18:05:30 -0400

    New Initial Commit

---
+++ Inclass07/app/src/main/java/mad/sis/uncc/inclass07/MainActivity.java
@@ -0,0 +1,14 @@
+package mad.sis.uncc.inclass07;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setTitle(R.string.title);
+    }
+}
